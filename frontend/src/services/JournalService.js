c09ac5e4354e1aa8f1b69d9fcffb52ba1a626297
shuylaw <107524206+shuylaw@users.noreply.github.com>
2023-10-06 11:41:57 +0800

    fix: change response.data to response

--- frontend/src/services/JournalService.js
+++ frontend/src/services/JournalService.js
@@ -3,27 +3,27 @@ import apiClient from "../lib/APIClient";
 const JournalService = {
     async getAll() {
         const response = await apiClient.get('/entries');
-        return response.data;
+        return response;
     },
 
     async get(id) {
         const response = await apiClient.get(`/entries/${id}`);
-        return response.data;
+        return response;
     },
 
     async create(data) {
         const response = await apiClient.post('/entries', data);
-        return response.data;
+        return response;
     },
 
     async update(id, data) {
         const response = await apiClient.patch(`/entries/${id}`, data);
-        return response.data;
+        return response;
     },
 
     async delete(id) {
         const response = await apiClient.delete(`/entries/${id}`);
-        return response.data;
+        return response;
     }
 };
 
