3c76e986dce79827cc6fa7976ad116109daa78ae
shuylaw <107524206+shuylaw@users.noreply.github.com>
2023-10-07 18:28:08 +0800

    feat: add header

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -3,7 +3,8 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
     return (
         <div className="flex flex-col items-center">
-            <nav className="navbar w-full m-0 p-0 justify-center menu menu-horizontal bg-base-200">
+            <nav className="flex flex-wrap navbar w-full m-0 p-0 justify-center menu menu-horizontal bg-base-200">
+                <div className="absolute top-0 left-0 font-bold ml-2 p-3 text-xl ">Mnemosyne</div>
                 <ul className="">
                     <li>
                         <Link to="/">Home</Link>
